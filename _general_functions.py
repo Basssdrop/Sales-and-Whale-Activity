39129763a40cfdc71ad700920647015eee50fdf0
Vedant <104223838+Basssdrop@users.noreply.github.com>
2023-11-21 15:16:07 +0530

    Update _general_functions.py

--- _general_functions.py
+++ _general_functions.py
@@ -6,3 +6,17 @@ def SaveH5(filename, data_array,dataset):
             dataset = file.create_dataset(f"{dataset}",data=data_array)
     except Exception as e:
         print(f"Error: {e}")
+
+def LoadH5(file_path, dataset_name):
+    try:
+        with h5py.File(file_path, 'r') as file:
+            # Check if the dataset exists
+            if dataset_name not in file:
+                raise KeyError(f"Dataset '{dataset_name}' not found in the HDF5 file.")
+
+            # Read the dataset into a NumPy array
+            dataset = file[dataset_name][...]
+            return dataset
+    except Exception as e:
+        print(f"Error reading HDF5 dataset: {str(e)}")
+        return None
